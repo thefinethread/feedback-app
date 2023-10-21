c2528a3c95f6ed616b93cdafa96fc4c97411e7b4
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-21 10:24:55 +0530

    added json-server for mock backend and added CRUD functionalities to feedback app-get/create/update/delete

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -1,35 +1,67 @@
-import { v4 as uuid4 } from 'uuid';
-import { createContext, useState } from 'react';
-import FeedbackData from '../../data/FeedbackData';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackContextProvider = ({ children }) => {
-  const [feedbacks, setFeedbacks] = useState(FeedbackData);
+  const [feedbacks, setFeedbacks] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     feedback: {},
     edit: false,
   });
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuid4();
-    setFeedbacks([newFeedback, ...feedbacks]);
+  useEffect(() => {
+    fetchFeedbacks();
+  }, []);
+
+  const fetchFeedbacks = async () => {
+    const res = await fetch('/feedbacks');
+    const data = await res.json();
+
+    setFeedbacks(data);
   };
 
-  const deleteFeedback = (id) => {
+  const addFeedback = async (newFeedback) => {
+    const res = await fetch('feedbacks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await res.json();
+
+    setFeedbacks([data, ...feedbacks]);
+  };
+
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
-      setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id));
+      const res = await fetch(`/feedbacks/${id}`, {
+        method: 'DELETE',
+      });
+      const statusCode = res.status;
+      if (statusCode === 200) {
+        setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id));
+      }
     }
   };
 
-  const updateFeedback = (feedback) => {
-    /* { ...item, ...feedback } means firstly "...item"  will spread the existing item
-       and then "...feedback" will override those fields which are being passed for update
-    */
+  const updateFeedback = async (feedback) => {
     if (feedbackEdit.edit) {
+      // call json server for update request
+      const res = await fetch(`/feedbacks/${feedback.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(feedback),
+      });
+      const data = await res.json();
+      /* { ...item, ...feedback } means firstly "...item"  will spread the existing item
+         and then "...feedback" will override those fields which are being passed for update
+      */
       setFeedbacks(
         feedbacks.map((item) =>
-          item.id === feedback.id ? { ...item, ...feedback } : item
+          item.id === data.id ? { ...item, ...data } : item
         )
       );
       setFeedbackEdit({ feedback: {}, edit: false });
